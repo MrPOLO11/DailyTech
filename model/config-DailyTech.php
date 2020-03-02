4bc570c43868fb50737c908c8061545a212952a4
Marcos Rivera <mrivera22@mail.greenriver.edu>
2020-03-02 14:40:06 -0500

    Corrected path in login credentials to correct database.

--- model/config-DailyTech.php
+++ model/config-DailyTech.php
@@ -2,6 +2,6 @@
 
 //config-student.php
 
-define("DB_DSN", "mysql:dbname=marcosri_DailyTech");
+define("DB_DSN", "mysql:dbname=marcosri_dailyTech");
 define("DB_USERNAME", "marcosri_grcuser");
 define("DB_PASSWORD", '2GMryy7d!gwy');
