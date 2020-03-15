041e3355e7a81a34f46fd54d8e6403d9faa6635e
Marcos Rivera <mrivera22@mail.greenriver.edu>
2020-03-15 19:24:27 -0400

    Added validation to email, password and ensured name is just alphabetical.

--- model/validator.php
+++ model/validator.php
@@ -16,16 +16,34 @@ class Validator
     public function validSignup()
     {
         $this->validName($_POST['name']);
-        //$this->validEmail();
-        //$this->validPassword();
+        $this->validEmail($_POST['email']);
+        $this->validPassword($_POST['email'], $_POST['password']);
     }
 
     private function validName($name)
     {
         if(empty($name)) {
             $this->_errors['name'] = "*Please provide a name";
-        } else if(!ctype_alpha($name)) {
+        } else if(!ctype_alpha(preg_replace('/\s+/', "", $name))) {
             $this->_errors['name'] = "*Name must consist of letters";
+        }
+    }
+
+    private function validEmail($email)
+    {
+        if(empty($email)) {
+            $this->_errors['email'] = "*Please provide an email";
+        } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->_errors['email'] = "*Must provide valid email";
+        }
+    }
+
+    private function validPassword($email, $password)
+    {
+        if(!empty($email) AND empty($password)) {
+            $this->_errors['password'] = "*Please provide a password";
+        } else if(strlen($password) < 8) {
+            $this->_errors['password'] = "*Password must be at least 8 characters";
         }
     }
 }
